621524e6731ad17b50cb09a9130c3be5987154c4
Tom Riddle <tomriddler@gmail.com>
2023-11-10 11:57:05 -0700

    more setup.ps1 fixes

--- setup.ps1
+++ setup.ps1
@@ -54,18 +54,34 @@ if (!(dir -ea 0 -LiteralPath "$thisPath\ArubaDownStatusMonitor")) { Write-Error 
 
     $baseProgramsPath0 = $match.Value | select -first 1
     $baseProgramsPath = $baseProgramsPath0 | split-path
-    $dashboardPath = "$baseProgramsPath\UniversalAutomation\Repository"
-    if(!(test-path $dashboardPath -ea 0)){Write-Error "Unable to find path to Powershell Universal dashboard directory at: $dashboardPath"; pause; return; exit;}
 
-    $dashboardsFile = "$baseProgramsPath\UniversalAutomation\Repository\dashboards.ps1"
+    $repoPath = "$baseProgramsPath\UniversalAutomation\Repository"
+    if(!(test-path $repoPath -ea 0)){mkdir $dashboardPath -force -verbose}
+    #Write-Error "Unable to find path to Powershell Universal dashboard directory at: $repoPath"; pause; return; exit;}
+
+    $dashboardPath = "$baseProgramsPath\UniversalAutomation\Repository\dashboards"
+    if(!(test-path $dashboardPath -ea 0)){mkdir $dashboardPath -force -verbose}
+    
+    $unidotpath = "$baseProgramsPath\UniversalAutomation\Repository\.universal"
+    if(!(test-path $unidotpath -ea 0)){mkdir $unidotpath -force -verbose}
+    
+
+    $dashboardsFile1 = "$baseProgramsPath\UniversalAutomation\Repository\dashboards.ps1"
     if(!(test-path $dashboardsFile -ea 0)){'' > $dashboardsFile}
+
+    $dashboardsFile2 = "$baseProgramsPath\UniversalAutomation\Repository\dashboards.ps1"
+    if(!(test-path $dashboardsFile2 -ea 0)){'' > $dashboardsFile2}
     #Write-Error "Unable to find expected file at: $dashboardPath"; pause; return; exit;}
 
     #---------------------------
 
     # enable dash
-    $dashInstalled = gc $dashboardsFile | ?{$_ -like "*dashboards\ArubaDownStatusMonitor\ArubaDownStatusMonitor.ps1*"}
-    if(!$dashInstalled){'New-PSUApp -Name "ArubaDownStatusMonitor" -FilePath "dashboards\ArubaDownStatusMonitor\ArubaDownStatusMonitor.ps1" -BaseUrl "/ArubaMonitor" -Authenticated -AutoDeploy' >> $dashboardsFile}else{write-host "Found installed dashboard at: $dashboardsFile"}
+    $dashInstalled = gc $dashboardsFile1 | ?{$_ -like "*dashboards\ArubaDownStatusMonitor\ArubaDownStatusMonitor.ps1*"}
+    if(!$dashInstalled){'New-PSUApp -Name "ArubaDownStatusMonitor" -FilePath "dashboards\ArubaDownStatusMonitor\ArubaDownStatusMonitor.ps1" -BaseUrl "/ArubaMonitor" -Authenticated -AutoDeploy' >> $dashboardsFile1}else{write-host "Found installed dashboard at: $dashboardsFile1"}
+
+    $dashInstalled2 = gc $dashboardsFile2 | ?{$_ -like "*dashboards\ArubaDownStatusMonitor\ArubaDownStatusMonitor.ps1*"}
+    if(!$dashInstalled2){'New-PSUApp -Name "ArubaDownStatusMonitor" -FilePath "dashboards\ArubaDownStatusMonitor\ArubaDownStatusMonitor.ps1" -BaseUrl "/ArubaMonitor" -Authenticated -AutoDeploy' >> $dashboardsFile2}else{write-host "Found installed dashboard at: $dashboardsFile2"}
+
 
     $dashboardAppDir = "$dashboardPath\dashboards"
     if(!(test-path $dashboardAppDir -ea 0)){Write-Error "Unable to find expected path: $dashboardAppDir"; pause; return; exit;}
